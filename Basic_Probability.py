20850a20bbe2e92c9d6b365ae9af7767b0ce9aa6
gaurav-120-bmsce <157561815+gaurav-120-bmsce@users.noreply.github.com>
2024-07-23 08:36:12 +0530

    Update Basic_Probability.py

--- Basic_Probability.py
+++ Basic_Probability.py
@@ -1,20 +1,20 @@
-xw = 5
-xb = 4
-x = xw+xb
-pxw = xw/x
-pxb = xb/x
+white_balls_x = 5
+black_balls_x = 4
+total_balls_x = white_balls_x + black_balls_x
+prob_white_x = white_balls_x / total_balls_x
+prob_black_x = black_balls_x / total_balls_x
 
-yw = 7
-yb = 6
-y = yw+yb
-pyw = yw/y
-pyb = yb/y
+white_balls_y = 7
+black_balls_y = 6
+total_balls_y = white_balls_y + black_balls_y
+prob_white_y = white_balls_y / total_balls_y
+prob_black_y = black_balls_y / total_balls_y
 
-newy = y+1
+new_total_balls_y = total_balls_y + 1
 
-probbfromyifw = yb/newy
-probbfromyifb = (yb+1)/newy
+prob_black_from_y_if_white = black_balls_y / new_total_balls_y
+prob_black_from_y_if_black = (black_balls_y + 1) / new_total_balls_y
 
-totalprob = probbfromyifb*pxb + probbfromyifw*pxw
+total_prob_black_from_y = prob_black_from_y_if_black * prob_black_x + prob_black_from_y_if_white * prob_white_x
 
-print("Probability of getting black from Y : ",totalprob)
+print(total_prob_black_from_y)
